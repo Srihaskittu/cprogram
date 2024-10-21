34b98b4c59ed75bda23194e619e07baee345d8e8
Srihaskittu <srihaskittu@gmail.com>
2024-10-21 11:31:08 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter2 numbers:");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("the sum is %d",c);
+}
