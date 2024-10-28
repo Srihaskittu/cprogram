b8dca9668b224c2191e8eb08ccef8303de6b88c4
Srihaskittu <srihaskittu@gmail.com>
2024-10-28 12:26:21 +0530

    Update quadratic.c

--- quadratic.c
+++ quadratic.c
@@ -2,24 +2,22 @@
 #include<math.h>
 void main()
 {
-int a,b,c,d;
-float r1,r2;
+int a,b,c;
+float r1,r2,d;
 printf("enter a,b,c:");
-scanf("%d,%d,%d",&a,&b,&c);
+scanf("%d%d%d",&a,&b,&c);
 d=((b*b)-(4*a*c));
-r1=(-b+sqrt(d)/2*a);
-r2=(-b+sqrt(d)/2*a);
-if(d>0)
-{
-printf("%f%f",&r1,&r2);
+if(d>0){
+r1=(-b+sqrt(d))/2*a;
+r2=(-b-sqrt(d))/2*a;
+printf("the real roots = %f%f", r1, r2);
 }
-else if
-(d<0)
-{
+else if  (d==0){
+r1=-b/(2*a);
+r2=-b/(2*a);
+printf("roots are equal = %f%f", r1, r2);
+}
+else{
     printf("roots are imaginary");
-}
-else
-{
-    printf("roots are real and equal");
 }
 }
