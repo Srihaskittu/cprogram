0c46f9ee2e11b8fdbb6c46764d957ce5f8ba252b
Srihaskittu <srihaskittu@gmail.com>
2024-12-30 12:26:07 +0530

    Create oppositepyramids.c

---
+++ oppositepyramids.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k,l;
+    for(i=1;i<=5;++i)
+    {
+        for(j=1;j<=i;++j)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+     for(k=1;k<=5;++k)
+       {
+        for(l=5;l>=k;--l)
+        {
+            printf("*");
+        }
+        printf("\n");
+     }
+    }
