ee1af974310842b08026edeb1cfba24df8fd34b8
Srihaskittu <srihaskittu@gmail.com>
2024-12-30 11:34:44 +0530

    Update characterspyramid.c

--- characterspyramid.c
+++ characterspyramid.c
@@ -2,14 +2,12 @@
 void main()
 {
     int i,j;
-    char n='A';
     for(i=0;i<=5;++i)
     {
-        for(j=0;j<=i;++j)
+        for(j=0;j<=5;++j)
         {
-            printf("%c",n);
+            printf("*");
         }
-        n++;
         printf("\n");
     }
 }
